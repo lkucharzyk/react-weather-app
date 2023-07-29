bae1eb6b3679f5946e4b44be463a71594b7ba168
Łukasze <lukaszkucharzyk17@gmail.com>
2023-07-29 09:50:54 +0200

    upgrades for Current weather and Daily Forecast modules

--- src/components/CurrentW.js
+++ src/components/CurrentW.js
@@ -5,13 +5,14 @@ export const CurrentW = ({currentWeather}) => {
   // useEffect(() =>{
   //   console.log(currentWeather.current);
   // })
-
+  const {current} = currentWeather
   return (
     <section className='main-module'>
         <h2>Current Weather</h2>
         <ul>
-            <li><i class="fa-solid fa-temperature-low"/>{currentWeather.current.temp_c} &#176;C</li>
-            <li><i class="fa-solid fa-wind"/>{currentWeather.current.wind_kph} km/H</li>
+            <li><i class="fa-solid fa-temperature-low"/>{current.temp_c} &#176;C</li>
+            <li><i class="fa-solid fa-droplet"></i>{current.precip_mm} mm</li>
+            <li><i class="fa-solid fa-wind"/>{current.wind_kph} km/H</li>
         </ul>
     </section>
   )
--- src/components/ForecastDay.js
+++ src/components/ForecastDay.js
@@ -1,12 +1,20 @@
 import React from 'react'
 
 export const ForecastDay = ({dayForecast}) => {
+  const {day} = dayForecast 
   return (
-    <section className='sub-module'>
-        <h5>{dayForecast.date}</h5>
+    <section className='sub-module forecast-day'>
+        <div className="header">
+          <div className="text">
+            <h5>{dayForecast.date}</h5>
+            <p>{day.condition.text}</p>
+          </div>
+           <img src={dayForecast.day.condition.icon} alt="" srcset="" />
+        </div>
+        
         <ul>
-            <li><i class="fa-solid fa-temperature-low"/>{dayForecast.day.avgtemp_c} &#176;C</li>
-            <li><i class="fa-solid fa-droplet"></i>{dayForecast.day.totalprecip_mm} mm</li>
+            <li><i class="fa-solid fa-temperature-low"/>{day.avgtemp_c} &#176;C</li>
+            <li><i class="fa-solid fa-droplet"></i>{day.totalprecip_mm} mm</li>
         </ul>
     </section>
   )
