6513f4f67ac478dc23c6ffa2451bbe6816181b67
Łukasze <lukaszkucharzyk17@gmail.com>
2023-08-03 13:12:03 +0200

    prod-2

--- src/components/ForecastHour.js
+++ src/components/ForecastHour.js
@@ -5,7 +5,7 @@ export const ForecastHour = ({hour}) => {
   const date = new Date();
   const currentHour = date.getHours();
 
-  return (<section className={`forecast-hour ${hour.time.slice(11, 13) ==currentHour ? 'current-hour' : ''}`}>
+  return (<section className={`forecast-hour ${+hour.time.slice(11, 13) ==currentHour ? 'current-hour' : ''}`}>
   
             <h5>{hour.time.slice(11, 13)} </h5>
             <div><i class="fa-solid fa-temperature-low"/>{hour.temp_c} &#176;C</div>
